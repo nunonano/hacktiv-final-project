e97bfb026fd0b9e80c8c0e419661786f2433714e
nunonano <wisnushapta@gmail.com>
2021-12-09 20:15:57 +0700

    testing deploy

--- app/database.go
+++ app/database.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/nunonano/hacktiv-final-project/model/entity"
 	"gorm.io/driver/postgres"
@@ -10,19 +9,19 @@ import (
 )
 
 var (
-// host     = "0.0.0.0"
-// user     = "postgres"
-// password = "secret"
-// dbName   = "hacktiv_db"
-// port     = "5432"
+	host     = "ec2-54-157-113-118.compute-1.amazonaws.com"
+	dbName   = "d1frg8fdqfata0"
+	user     = "zvmehrtxtwnmea"
+	password = "4af7ba68f834dba2c0fccfc4cc8f348b09f2b52050fb47b532afcb5bbed1a61e"
+	port     = "5432"
 )
 
 func InitDB() *gorm.DB {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	port := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
+	// host := os.Getenv("DB_HOST")
+	// user := os.Getenv("DB_USER")
+	// password := os.Getenv("DB_PASSWORD")
+	// port := os.Getenv("DB_PORT")
+	// dbName := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s TimeZone=Asia/Jakarta", host, user, password, dbName, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
